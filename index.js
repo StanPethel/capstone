2d948eb69bac39e79b1d5b4c096422e1b4727047
Stanley Pethel <stanleypethel@yahoo.com>
2022-01-06 23:11:00 -0500

    updated port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const connectDb = require("./db/db");
 const usersRouter = require("./routes/users");
 const productRouter = require("./routes/product");
-const authRoute = require("./middleware/auth");
+const auth = require("./middleware/auth");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
 const stripeRoute = require("./routes/stripe");
@@ -13,14 +13,14 @@ connectDb();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/auth", authRoute);
+app.use("/api/auth", auth);
 app.use('/api/product', productRouter);
 app.use(`/api/users`, usersRouter);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/checkout", stripeRoute);
 
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server started on port: ${port}`);
 });
