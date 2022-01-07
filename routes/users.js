43eea27b9449bad26f974916e50e5a298289a251
Stanley Pethel <stanleypethel@yahoo.com>
2022-01-06 23:48:10 -0500

    updated and corrected routes and token issues

--- routes/users.js
+++ routes/users.js
@@ -1,91 +1,87 @@
-const { User, validateLogin, validateUser } = require("../models/user");
+const User = require("../models/User");
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
 
-const auth = require("../middleware/auth");
-const admin = require("../middleware/admin");
+const router = require("express").Router();
 
-const bcryptjs = require("bcryptjs");
-const express = require("express");
-const router = express.Router();
-
-//* POST register a new user
-router.post("/register", async (req, res) => {
-  try {
-    const { error } = validateUser(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-
-    let user = await User.findOne({ email: req.body.email });
-    if (user)
-      return res.status(400).send(`Email ${req.body.email} already claimed!`);
-
-    const salt = await bcryptjs.genSalt(10);
-    user = new User({
-      name: req.body.name,
-      email: req.body.email,
-      password: await bcryptjs.hash(req.body.password, salt),
-      isAdmin: req.body.isAdmin,
-    });
-
-    await user.save();
-    const token = user.generateAuthToken();
-    return res
-      .header("x-auth-token", token)
-      .header("access-control-expose-headers", "x-auth-token")
-      .send({
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        isAdmin: user.isAdmin,
-      });
-  } catch (ex) {
-    return res.status(500).send(`Internal Server Error: ${ex}`);
-  }
-});
-//* POST a valid login attempt
-//! when a user logs in, a new JWT token is generated and sent if their email/password credentials are correct
-router.post("/login", async (req, res) => {
-  try {
-    const { error } = validateLogin(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-
-    let user = await User.findOne({ email: req.body.email });
-    if (!user) return res.status(400).send(`Invalid email or password.`);
-
-    const validPassword = await bcryptjs.compare(
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
       req.body.password,
-      user.password
+      process.env.PASS_SEC
+    ).toString();
+  }
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
     );
-    if (!validPassword)
-      return res.status(400).send("Invalid email or password.");
-
-    const token = user.generateAuthToken();
-    return res.send(token);
-  } catch (ex) {
-    return res.status(500).send(`Internal Server Error: ${ex}`);
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
-//* Get all users
-router.get("/", async (req, res) => {
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
   try {
-    const users = await User.find();
-    return res.send(users);
-  } catch (ex) {
-    return res.status(500).send(`Internal Server Error: ${ex}`);
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
-//* DELETE a single user from the database
-router.delete("/:userId", [auth, admin], async (req, res) => {
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
   try {
-    const user = await User.findById(req.params.userId);
-    if (!user)
-      return res
-        .status(400)
-        .send(`User with id ${req.params.userId} does not exist!`);
-    await user.remove();
-    return res.send(user);
-  } catch (ex) {
-    return res.status(500).send(`Internal Server Error: ${ex}`);
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  const query = req.query.new;
+  try {
+    const users = query
+      ? await User.find().sort({ _id: -1 }).limit(5)
+      : await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
+  const date = new Date();
+  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+  try {
+    const data = await User.aggregate([
+      { $match: { createdAt: { $gte: lastYear } } },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: 1 },
+        },
+      },
+    ]);
+    res.status(200).json(data)
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
